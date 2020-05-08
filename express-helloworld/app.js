cc450c65f9fce44b4e82a37ddb330d1b53515042
c-hobbins <hobbins.chad@gmail.com>
2020-05-07 21:20:15 -0400

    Added the /client resource path

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello Chad!\n');
 });
 
+app.get('/client', function (req, res) {
+  res.send('Yes this would be a JSON client object...');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
